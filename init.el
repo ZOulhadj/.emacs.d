20243fc2cf5c9ae00f29c652f3524f28fefe7f5f
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2021-04-25 14:47:56 +0200

    Now using customisation interface as it's much more simpler and less code

---
+++ init.el
@@ -0,0 +1,33 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(backup-directory-alist '(("" . "~/.config/emacs/backups")))
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(confirm-kill-emacs 'y-or-n-p)
+ '(confirm-kill-processes nil)
+ '(create-lockfiles nil)
+ '(cursor-in-non-selected-windows nil)
+ '(delete-by-moving-to-trash t)
+ '(global-auto-revert-mode t)
+ '(global-subword-mode t)
+ '(help-window-select t)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-echo-area-message "zakariya")
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice t)
+ '(initial-scratch-message nil)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(vc-make-backup-files t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "GOOG" :family "Consolas")))))
